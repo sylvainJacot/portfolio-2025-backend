e5ad264d842a8ee3bf4821cb7cd2e4c6f4dce47d
Sylvain <jct.sylvain@gmail.com>
2025-04-03 22:14:09 +0200

    port change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const chatRoutes = require('./routes/chat');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors({
